29dc9d8f0f2c542e010877a719260cfccaf10d3a
Maleda Tessema <mal.bt27@gmail.com>
2020-06-05 20:46:24 -0400

    revised

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,8 +1,9 @@
-for x in range(101):
-    print(x)
-    if (x % 3 == 0):
-        print("Fizz")
-    if (x % 5 == 0):
-        print("Buzz")
-    if (x % 3 == 0 and x % 5 == 0):
+for x in range(1, 100):
+    if (x % 15== 0):
         print("FizzBuzz")
+    elif (x % 3 == 0):
+        print("Fizz")
+    elif (x % 5 == 0):
+        print("Buzz")
+    else:
+        print(x)
